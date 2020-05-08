6bd953cc80ea07ae95b40cc1331e1d2dad311554
Suman Paul <suman.paul@optus.com.au>
2020-05-08 23:14:22 +0530

    adding hapi and first route

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/tables',
+        handler: function (request, h) {
+    
+            const payload = request.payload;
+    
+            return `Welcome ${payload.hi}!`;
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
